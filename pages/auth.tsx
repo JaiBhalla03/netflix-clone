1c044286315602f830e9db56443d6f69c22da0d0
jai <jai.bhalla2021@vitstudent.ac.in>
2023-03-24 17:54:12 +0530

    Adding the login page and the connecting the prisma to intigrate the database with it and also adding the nextauth with it

---
+++ pages/auth.tsx
@@ -0,0 +1,96 @@
+import Input from "@/components/Input";
+import {useCallback, useState} from "react";
+import axios from 'axios';
+import {signIn} from "next-auth/react";
+import {useRouter} from "next/router";
+
+const Auth = ()=>{
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [variant, setVariant] = useState('login');
+    const router = useRouter();
+    const toggleVariant = useCallback(()=>{
+        setVariant((currentVariant)=>currentVariant==='login' ? 'register'  : 'login')
+    }, []);
+    const Login = useCallback(async()=>{
+        try{
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+            await router.push('/');
+        }
+        catch(err){
+            console.log(err)
+        }
+    },[email, password, router])
+    const register = useCallback(async()=>{
+        try{
+            await axios.post('/api/register', {
+                email, name, password
+            });
+            await Login()
+        }
+        catch(err){
+            console.log(err)
+        }
+    },[email, name, password, Login])
+
+    return(
+      <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+          <div className={'bg-black w-full h-full lg:bg-opacity-50'}>
+              <nav className={'px-12 py-5'}>
+                  <img src={'/images/logo.png'} alt={''} className={'h-12'}/>
+              </nav>
+              <div className={'flex justify-center'}>
+                  <div className={'bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'}>
+                      <h2 className={'text-white text-4xl mb-8 font-semibold'}>
+                          {variant === 'login' ? 'Sign in' : 'Register'}
+                      </h2>
+                      <div className={'flex flex-col gap-4'}>
+                          {
+                              variant === 'register' && (
+                                  <Input
+                                      label={'Username'}
+                                      onChange={(ev)=>setName(ev.target.value)}
+                                      id={'name'}
+                                      type={'text'}
+                                      value={name}
+                                  />
+                              )
+                          }
+                          <Input
+                              label={'Email'}
+                              onChange={(ev)=>setEmail(ev.target.value)}
+                              id={'email'}
+                              type={'email'}
+                              value={email}
+                          />
+                          <Input
+                              label={'Password'}
+                              onChange={(ev)=>setPassword(ev.target.value)}
+                              id={'password'}
+                              type={'password'}
+                              value={password}
+                          />
+                      </div>
+                      <button onClick={variant === 'login' ? Login : register} className={'bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'}>
+                          {variant === 'login' ? 'Login' : 'Sign Up'}
+                      </button>
+                      <p className={'text-neutral-500 mt-12'}>
+                          {variant === 'login' ? 'First time using Netflix?': 'Already have an account'}
+                          <span onClick={toggleVariant} className={'text-white ml-1 hover:underline cursor-pointer'}>
+                              {variant === 'login' ? 'Create an account' : 'Login'}
+                          </span>
+                      </p>
+                  </div>
+              </div>
+          </div>
+      </div>
+    );
+}
+
+export default Auth;
